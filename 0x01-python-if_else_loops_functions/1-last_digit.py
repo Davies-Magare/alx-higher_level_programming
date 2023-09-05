a6c54086c6321459e2d3571e117a4773cd2daa51
Davies Magare <davieskamanda@gmail.com>
2023-09-05 07:59:29 -0700

    Corrected case for negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,8 @@ if temp < 0:
     temp *= -1
 str1 = "Last digit of "
 last = temp % 10
+if number < 0:
+    last *= -1
 if last > 5:
     print(f"{str1}{number} is {last} and is greater than 5")
 elif last == 0:
