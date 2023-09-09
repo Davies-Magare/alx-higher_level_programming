44dc116dd37da28df55bf5b74149206775b576ff
Davies Magare <davieskamanda@gmail.com>
2023-09-09 08:16:54 -0700

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        print()
+        return
+    i = 0
+    while i < len(matrix):
+        j = 0
+        while j < len(matrix[i]):
+            if j < len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=' ')
+            else:
+                print("{:d}".format(matrix[i][j]))
+            j += 1
+        i += 1
