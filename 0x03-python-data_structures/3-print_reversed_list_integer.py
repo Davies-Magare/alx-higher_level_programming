eb23796458268cf2ae7e01b4a168d84e0f836f54
Davies Magare <davieskamanda@gmail.com>
2023-09-09 00:58:35 -0700

    Corrected synta error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     rev_list = reversed(my_list)
     for integer in rev_list:
-        print("{}".format(integer))
+        print("{:d}".format(integer))
