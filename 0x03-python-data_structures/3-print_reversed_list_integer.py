48a307e393ea522b9a70626924500453975c5d29
Davies Magare <davieskamanda@gmail.com>
2023-09-09 00:57:14 -0700

    Corrected syntax

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     rev_list = reversed(my_list)
     for integer in rev_list:
-        print("{:d}".format(integer))
+        print("{}".format(integer))
