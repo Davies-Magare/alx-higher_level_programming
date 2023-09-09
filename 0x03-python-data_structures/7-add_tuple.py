bf0f123058b976eea9dfc319deea332d8932387a
Davies Magare <davieskamanda@gmail.com>
2023-09-09 13:03:39 -0700

    Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        t = 0, 0
+    elif len(tuple_a) == 1 and len(tuple_b) == 0:
+        t = tuple_a[0], 0
+    elif len(tuple_a) == 1 and len(tuple_b) == 1:
+        t = tuple_a[0] + tuple_b[0], 0
+    elif len(tuple_a) == 0 and len(tuple_b) == 1:
+        t = tuple_b[0], 0
+    elif len(tuple_a) == 1 and len(tuple_b) == 2:
+        t = tuple_a[0] + tuple_b[0], tuple_b[1]
+    elif len(tuple_a) == 2 and len(tuple_b) == 1:
+        t = tuple_a[0] + tuple_b[0], tuple_a[1]
+    elif len(tuple_a) == 2 and len(tuple_b) == 0:
+        t = tuple_a[0], tuple_a[1]
+    elif len(tuple_b) == 2 and len(tuple_a) == 0:
+        t = tuple_b[0], tuple_b[1]
+    elif len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        t = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return t
