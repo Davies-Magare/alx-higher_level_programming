423e217c14fae966fe9161476b50d5d7c21158c7
Davies Magare <davieskamanda@gmail.com>
2023-09-13 22:28:00 -0700

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or roman_string is None:
+        return 0
+    rom_str = roman_string
+    d = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+    number = 0
+    i = 0
+    while i < len(rom_str):
+        if i < len(rom_str) - 1 and
+        rom_str[i] == 'I' and
+        rom_str[i + 1] == 'X':
+            number += 9
+            i += 2
+        else:
+            number += d[rom_str[i]]
+            i += 1
+    return number
