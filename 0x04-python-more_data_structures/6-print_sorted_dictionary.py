4069ce116dd82b9534390e2eb57ca5d29516eeb1
Davies Magare <davieskamanda@gmail.com>
2023-09-13 19:47:06 -0700

    Task 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    lst = list(a_dictionary)
+    l.sort()
+    new_dict = []
+    for item in l:
+        tup = item, a_dictionary[item]
+        print(tup)
