16d24c1d7b5e8bc8cf7c14174b54a5bd1cbd11c0
Davies Magare <davieskamanda@gmail.com>
2023-09-13 20:06:01 -0700

    Added condition

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
+    for item in dictionary:
+        if item == key:
+            del a_dictionary[key]
     return a_dictionary
