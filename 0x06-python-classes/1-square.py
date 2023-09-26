2f08873250fea1d84e31d62b3bac9c6221d338b0
Davies Magare <davieskamanda@gmail.com>
2023-09-26 18:48:33 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module documentation
+
+This module defines a square
+"""
+
+
+class Square:
+    """This is a class that describes a square"""
+    def __init__(self, size):
+        self.__size = size
