f20386cc0e0d1885515dc961547f4b6af76523ef
Davies Magare <davieskamanda@gmail.com>
2023-09-26 04:25:55 +0000

    Task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,6 +13,8 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print('out of range')
             result.append(0)
+        except Exception:
+            result.append(0)
         finally:
             pass
     return result
