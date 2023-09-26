7782b62c17138ad3f59f2b9b78c8282fca808ce3
Davies Magare <davieskamanda@gmail.com>
2023-09-26 02:43:44 +0000

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     c = 0
     try:
         c = a / b
-    except Exception:
+    except ZeroDivisionError:
         return None
     finally:
         print("Inside result: {}".format(c))
