7c5588b48dffd6764d71bfc4277eeb6ead5c8e48
Davies Magare <davieskamanda@gmail.com>
2023-10-02 20:33:29 +0000

    Task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+This module describes an empty class
+"""
+
+
+class Rectangle:
+    """Rectangle:Describes a rectangle
+    This class describes a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """width: retrieves the width of the rectangle
+        Returns: The width of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter: sets and validates the width of
+        the rectangle
+        Parameters
+        __________
+        value: The width of the rectangle passed
+        Returns: The validated width of the rectangle
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """gets the height of the rectangle
+        Returns: The height of the rectangle
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter: sets and validates the height of the
+        rectangle
+        Parameters
+        __________
+        value: The height of the rectangle passed
+        Returns: The validated height of the rectangle
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def perimeter(self):
+        """perimeter: returns the perimeter of the rectangle"""
+        if (self.__width == 0 and self.__height == 0):
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def area(self):
+        """area: returns the area of the rectangle"""
+        return self.__width * self.__height
