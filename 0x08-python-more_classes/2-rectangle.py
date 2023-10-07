eb5fb9bd96ee367f2be9d38745310cda7e50b45d
Davies Magare <davieskamanda@gmail.com>
2023-10-07 15:59:08 +0000

    Corrected case for 0 height/width

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -61,7 +61,7 @@ class Rectangle:
 
     def perimeter(self):
         """perimeter: returns the perimeter of the rectangle"""
-        if (self.__width == 0 and self.__height == 0):
+        if (self.__width == 0 or self.__height == 0):
             return 0
         return 2 * (self.__width + self.__height)
 
