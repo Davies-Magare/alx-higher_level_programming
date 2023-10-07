73d454716d91ed992eaec4eb42a661ff195e0a23
Davies Magare <davieskamanda@gmail.com>
2023-10-07 15:43:00 +0000

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,60 +1,55 @@
 #!/usr/bin/python3
 """
-This module describes an empty class
+This is the rectangle module
 """
 
 
 class Rectangle:
-    """Rectangle:Describes a rectangle
-    This class describes a rectangle
-    """
+    """ A class that describes a rectangle"""
+
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """
+        init: class constructor
+        width: The width of the rectangle
+        height: The height of the rectangle
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """width: retrieves the width of the rectangle
-        Returns: The width of the rectangle
+        """
+        width: retrieves the width
         """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """setter: sets and validates the width of
-        the rectangle
-        Parameters
-        __________
-        value: The width of the rectangle passed
-        Returns: The validated width of the rectangle
+        """
+        width: sets the width
+        value: The value to set the width
         """
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif width < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """gets the height of the rectangle
-        Returns: The height of the rectangle
         """
-
+        height: retrieves the height of the rectangle
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """setter: sets and validates the height of the
-        rectangle
-        Parameters
-        __________
-        value: The height of the rectangle passed
-        Returns: The validated height of the rectangle
         """
-
+        height: Validates and sets the height value
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif height < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
