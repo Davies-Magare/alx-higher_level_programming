d657e8436b7600074c3427b8b35685c34dfa3952
Davies Magare <davieskamanda@gmail.com>
2023-10-17 10:14:12 +0000

    made style changes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,5 +100,5 @@ class Rectangle(Base):
     def __str__(self):
         """str: returns string representation of rectangle"""
 
-        return('[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.__x,
-                                        self.__y, self.__width, self.__height))
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(
+                self.id, self.__x, self.__y, self.__width, self.__height)
