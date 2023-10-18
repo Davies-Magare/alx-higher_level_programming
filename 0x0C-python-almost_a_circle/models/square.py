2ad17e38d2c849b8b0a7682743a8771f36d6479e
Davies Magare <davieskamanda@gmail.com>
2023-10-18 06:16:37 +0000

    Task 11: Define Getter setter for square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,3 +15,21 @@ class Square(Rectangle):
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
                 self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """width: returns the size"""
+
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """width: sets the size"""
+
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        elif (size <= 0):
+            raise ValueError("width must be > 0")
+
+        self.width = size
+        self.height = size
