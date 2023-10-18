64a2d3cc4ad7703669f7775ef2fb483dec321625
Davies Magare <davieskamanda@gmail.com>
2023-10-18 08:12:41 +0000

    Task 13: return square as a dictionary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -57,3 +57,16 @@ class Square(Rectangle):
                 self.x = kwargs['x']
             if 'y' in kwargs:
                 self.y = kwargs['y']
+
+    def to_dictionary(self):
+        """
+        to_dictionary: returns dictionary
+        representation of square
+        """
+
+        return {
+                'id': self.id,
+                'size': self.width,
+                'x': self.x,
+                'y': self.y
+            }
