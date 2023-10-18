b434bea7c5243d3b1721975710f9743208a0bebb
Davies Magare <davieskamanda@gmail.com>
2023-10-18 15:41:53 +0000

    Task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,37 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        save_to_file: saves list of instances to a file
+
+        Attributes
+        ==========
+        list_objs: A list of class isntances to be serialized
+        """
+        class_name = type(list_objs[0]).__name__
+        dict_obj = []
+        for item in list_objs:
+            if class_name == 'Rectangle':
+                dictnr = {
+                        'id': item.id,
+                        'width': item.width,
+                        'height': item.height,
+                        'x': item.x,
+                        'y': item.y
+                        }
+                dict_obj.append(dictnr)
+            elif class_name == 'Square':
+                dictnr = {
+                        'id': item.id,
+                        'size': item.width,
+                        'x': item.x,
+                        'y': item.y
+                        }
+                dict_obj.append(dictnr)
+        json_str_obj = cls.to_json_string(dict_obj)
+        filename = class_name + '.json'
+        with open(filename, "w") as f:
+            f.write(json_str_obj)
