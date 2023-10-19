43b38abc7efd49f9789e31a3253ab7d61966329a
Davies Magare <davieskamanda@gmail.com>
2023-10-19 15:53:51 +0000

    Added tests

--- 0x0C-python-almost_a_circle/pizza.py
+++
@@ -1,6 +0,0 @@
-class Pizza:
-    def __init__(self, ingredients):
-        self.ingredients = ingredients
-
-    def __repr__(self):
-        return f'Pizza({self.ingredients!r})'
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,24 +3,73 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
     def test_args(self):
-        first = Rectangle(4, 5)
-        self.assertEqual(first.id, 2)
-
-        second = Rectangle(4, [2, 3])
-        self.assertEqual(second.id, 3)
-
-        second = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(second.id, 12)
-
         second = Rectangle(10, 2, 0, 0)
-        self.assertEqual(second.id, 4)
+        self.assertEqual(second.id, 3)
+        self.assertEqual(second.width, 10)
+        self.assertEqual(second.height, 2)
+        self.assertEqual(second.x, 0)
+        self.assertEqual(second.y, 0)
+
+        first = Rectangle(10, 2, 0, 0, 79)
+        self.assertEqual(first.id, 79)
 
     def test_exception(self):
         with self.assertRaises(TypeError):
             Rectangle()
-        
         with self.assertRaises(TypeError):
-            Rectangle(4)
+            Rectangle((4, ))
+        with self.assertRaises(TypeError):
+            Rectangle({4: 'davies'})
+        with self.assertRaises(TypeError):
+            Rectangle([])
+        with self.assertRaises(ValueError):
+            Rectangle(-4, 3)
+        with self.assertRaises(ValueError):
+            Rectangle(4, -3)
+        with self.assertRaises(TypeError):
+            Rectangle(4, [], 1, 1, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, (1,), 1, 1, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, "1", 1, 1, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, {1: "st"}, 1, 1, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(4, 3, -2, 1, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(4, 3, 2, -1, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(0, 3, 2, 1, 0)
+        with self.assertRaises(ValueError):
+            Rectangle(3, 0, 2, 1, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4.5, 0, 2, 1, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 1.5, 0, 1, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 1, 1.5, 1, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 1, 1, 1.5, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 1, [], 1, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 1, {4: 'cat'}, 1, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 1, (2,), 1, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 1, 1, [], 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 1, 1, {4: 'dav'}, 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 1, 1, "1", 0)
+        with self.assertRaises(TypeError):
+            Rectangle(4, 1, "2", 1, 0)
+
+    def test_area(self):
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+    
+
 
     if __name__ == '__main__':
         unittest.main()
