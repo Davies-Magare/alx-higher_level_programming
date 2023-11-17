8e87758e6459f694ba91b367de2e44e38ba287d0
Davies Magare <davieskamanda@gmail.com>
2023-11-17 03:17:19 -0800

    Task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,6 @@
 -- Select cities of California
-SELECT id, name
-FROM cities
-WHERE id IN (
-	SELECT  id
-	FROM states
-	WHERE name = 'California'
-)
-ORDER BY id DESC;
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+AND states.name = 'California'
+ORDER BY cities.id DESC;
