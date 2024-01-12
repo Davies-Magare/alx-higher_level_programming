6503435d00ca7f24a425191fbb7cd33a503ac376
Davies Magare <davieskamanda@gmail.com>
2024-01-12 15:50:44 +0000

    Task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- Select cities of California that can be found in hbtn_0d_usa database
+SELECT id, name FROM cities WHERE state_id IN
+(SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
