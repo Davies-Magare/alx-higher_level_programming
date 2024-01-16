3ddc2cfb0c26145f6ab8a8dfa8fe852b5640b2a2
Davies Magare <davieskamanda@gmail.com>
2024-01-16 02:11:03 +0000

    Fixed code errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,5 @@
-"""This module lists all states from the database hbtn_0e_0_usa"""
-
 #!/usr/bin/python3
+"""This module lists all states from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
@@ -8,9 +7,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost",  port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_result = cur.fetchall()
     for row in query_result:
         print(row)
-    cur.close
-    conn.close
+    cur.close()
+    conn.close()
