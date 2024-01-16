584d745e3c8b0933f0d9ee1db6320b2c29b8ba64
Davies Magare <davieskamanda@gmail.com>
2024-01-16 14:24:56 +0000

    Task 5: List all cities in state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""List all cities in state passed in command line argument"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost",  port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT name from cities WHERE state_id IN \
+             (SELECT id from states WHERE name = %s)"
+    cur.execute(query, (sys.argv[4],))
+    query_result = cur.fetchall()
+    result_list = []
+    for row in query_result:
+        result_list.append(row[0])
+    for city in result_list:
+        if city != result_list[-1]:
+            print("{}, ".format(city), end="")
+        else:
+            print(city)
+    cur.close()
+    conn.close()
