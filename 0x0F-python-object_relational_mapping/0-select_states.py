698cf62914ba5883eccad60866bc3bb5353d30a4
Davies Magare <davieskamanda@gmail.com>
2024-01-16 02:04:09 +0000

    Fixed module documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
+"""This module lists all states from the database hbtn_0e_0_usa"""
+
 #!/usr/bin/python3
 import sys
 import MySQLdb
-""" This script lists all states from the database hbtn_0e_0_usa """
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost",  port=3306, user=sys.argv[1],
