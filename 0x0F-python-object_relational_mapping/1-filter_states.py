5aabdccd67659f3e86b8c5b509bbfc1ea7300fd5
Davies Magare <davieskamanda@gmail.com>
2024-01-16 02:23:45 +0000

    Fix code style

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module lists all states from the database hbtn_0e_0_usa that start with 'N'"""
+"""This module lists all states from the database hbtn_0e_0_usa
+that start with 'N'
+"""
 import sys
 import MySQLdb
 
@@ -7,7 +9,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost",  port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE
+                'N%' ORDER BY states.id ASC")
     query_result = cur.fetchall()
     for row in query_result:
         print(row)
