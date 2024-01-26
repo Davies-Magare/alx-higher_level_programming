53fb537b0fe637be86ab73ae09000f38b68b875a
Davies Magare <davieskamanda@gmail.com>
2024-01-26 00:28:25 +0000

    Task 0 Correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #This script sends a request to the given url and display the body size of the response
-if [ #$ -eq 1 ]
-then
-	curl -sI "$1" | grep -i Content-Length
+curl -sI "$1" | grep -i Content-Length
