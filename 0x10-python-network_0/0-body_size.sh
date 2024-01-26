78d6d7e0a77979cd89ef1f619f1eee75f22a9a9a
Davies Magare <davieskamanda@gmail.com>
2024-01-26 00:04:46 +0000

    Corrected task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
-#This script sends a request to the given url and displays
-#the body size of the response
+#This script sends a request to the given url and display the body size of the response
 curl -sI $1 | grep content-length
