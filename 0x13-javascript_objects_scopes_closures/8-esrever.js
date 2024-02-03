43cecc96a0155e51607288365e4f542bc201f3c9
Davies Magare <65121681+Davies-Magare@users.noreply.github.com>
2024-02-03 16:13:19 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-exports.esrever = function (list) {
-  const revList = [];
-  let i;
-  for (i = list.length - 1; i >= 0; i--) {
-    revList.push(list[i]);
-  }
-  return (revList);
-};
