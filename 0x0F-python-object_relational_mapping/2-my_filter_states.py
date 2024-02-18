588f29c7fb33264853b980963cb81ce822269ddd
Davies Magare <davieskamanda@gmail.com>
2024-02-18 12:32:41 +0000

    Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Select all states with names corresponding to commandline argument"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], password=sys.argv[2],
+                         database=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states WHERE NAME
+                    LIKE %s ORDER BY id ASC""", (sys.argv[4],))
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
+    cursor.close()
+    db.close()
