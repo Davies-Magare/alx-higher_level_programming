9cd53f6da62c8c09468ab8771453c75225dd12f7
Davies Magare <davieskamanda@gmail.com>
2024-02-18 12:16:52 +0000

    Corrected syntax

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          user=sys.argv[1], password=sys.argv[2],
                          database=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("""SELECT id, name FROM states WHERE NAME
+    cursor.execute("""SELECT * FROM states WHERE NAME
                     LIKE 'N%' ORDER BY id ASC""")
     results = cursor.fetchall()
     for result in results:
