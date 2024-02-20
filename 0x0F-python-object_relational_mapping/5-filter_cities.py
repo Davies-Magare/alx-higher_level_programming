6eca40d35f46854ee8d0c5d94767b765326230ab
Davies Magare <davieskamanda@gmail.com>
2024-02-20 14:03:40 +0000

    Task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+List all cities from database hbtn_0e_4_usa
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    def print_results(results):
+        if len(results) == 0:
+            return
+        else:
+            if len(results) == 1:
+                print(results[0][0])
+            else:
+                print("{}, ".format(results[0][0]), end="")
+            return print_results(results[1:])
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], password=sys.argv[2],
+                         database=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT name FROM cities WHERE state_id IN
+            (SELECT id FROM states WHERE name = %s)""", (sys.argv[4],))
+    results = cursor.fetchall()
+    print_results(results)
+    cursor.close()
+    db.close()
