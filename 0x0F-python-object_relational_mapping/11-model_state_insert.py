2e032aeda0433adbdd417e812c4cd8b3384d8250
Davies Magare <davieskamanda@gmail.com>
2024-02-21 15:56:28 +0000

    Task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,7 +8,9 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]),pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     lous = State(name='Louisiana')
     Session = sessionmaker(bind=engine)
@@ -16,7 +18,7 @@ if __name__ == "__main__":
     session.add(lous)
     session.commit()
     results = session.query(State.id).filter(State.name == "Louisiana").first()
-    if results == None:
+    if results is None:
         print("Nothing")
     else:
         print(results[0])
