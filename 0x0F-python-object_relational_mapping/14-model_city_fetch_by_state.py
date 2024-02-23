6a3fea52a03745c41639f1946cb8f1e58fe8d9ca
Davies Magare <davieskamanda@gmail.com>
2024-02-23 04:36:00 +0000

    Updated documentation for task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-List all city objects from database
+This module prints all City objects from the database
+hbtn_0e_14_usa
 """
 import sys
 from model_state import Base, State
