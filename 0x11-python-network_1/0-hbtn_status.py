afa935c99674ba4530ca24f8bf7a1cd6f712507e
Davies Magare <davieskamanda@gmail.com>
2024-03-02 11:38:40 +0000

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A python script that fetches 
+https://alx-intranet.hbtn.io/status
+"""
+from urllib.request import urlopen
+url = "https://alx-intranet.hbtn.io/status"
+with urlopen(url) as response:
+    body = response.read()
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+content = body.decode('utf-8')
+utf8 = "\t- utf8 content: {}".format(content)
+print(utf8)
