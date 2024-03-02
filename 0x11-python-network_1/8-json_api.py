00bc033ec32e04254c8b9440e2785b5515534edc
Davies Magare <davieskamanda@gmail.com>
2024-03-02 16:07:02 +0000

    Task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Take in a letter and send a post request to url
+with the letter as a parameter
+"""
+import requests
+import sys
+payload = {}
+arg = ""
+if len(sys.argv) >= 2:
+    arg = sys.argv[1]
+payload["q"] = arg
+url = "http://0.0.0.0:5000/search_user"
+try:
+    r = requests.post(url, data=payload)
+    try:
+        json_str = r.json()
+        if not json_str:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_str['id'], json_str['name']))
+    except Exception:
+        print("Not a valid Json")
+except Exception:
+    pass
